895ca7b04ded947f38c2d0e18377fbf9d11a29b4
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-09-23 15:36:29 +0300

    Add project files.

---
+++ Number Pyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+
+            int currentNumber = 1;
+            for (int rows = 1; rows <= num; rows++)
+            {
+                bool isFinis=false;
+
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write($"{currentNumber} ");
+                    currentNumber++;    
+                    if(currentNumber>num)
+                    {
+                        isFinis = true;
+                        break;
+                    }
+                    
+                }
+                Console.WriteLine();
+                if (isFinis)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
